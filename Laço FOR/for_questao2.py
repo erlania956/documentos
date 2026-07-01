d684afd4a16f3b665d907d4da016dec3328cec0c
Erlania neves <neveserlania@gmail.com>
2026-07-01 11:19:18 -0300

    laco de repetição for complento

---
+++ Laço FOR/for_questao2.py
@@ -0,0 +1,5 @@
+n = int(input("digite o valor de N: "))
+soma = 0
+for i in range(1, n+1):
+    soma+= i
+print(f" a soma dos numeros de 1 ate {n} é {soma}")
---
+++ Laço FOR/questaofor4.py
@@ -0,0 +1,3 @@
+numero = int(input("digite um numero: "))
+for i in range(1, n + 1):
+    print(f" o fatorial de {numero} x {i} = {numero * i}")
