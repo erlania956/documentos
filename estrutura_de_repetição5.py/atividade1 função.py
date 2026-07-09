6286fac80a29025b2f7a47fbd996454ae6fd3933
Erlania neves <neveserlania@gmail.com>
2026-07-09 11:02:27 -0300

    atividade de função

---
+++ estrutura_de_repetição5.py/atividade1 função.py
@@ -0,0 +1,6 @@
+def arg():
+    arg1 = int(input("Digite o primeiro arg: "))
+    arg2 = int(input("Digite o segundo arg: "))
+    arg3 = int(input("Digite o terceiro arg: "))
+    return arg1 + arg2 + arg3
+print( arg ())
